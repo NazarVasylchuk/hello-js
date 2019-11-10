0ab47836c52ac26b74ecf85984246b9beeb333ab
NazarVasylchuk <nazarij554@gmail.com>
2019-11-10 16:01:25 +0200

    lesson07

--- public/orchestra.js
+++ public/orchestra.js
@@ -1,36 +1,24 @@
-function playS( id ) {
-        const audio = document.getElementById( 'snd' +id );
-        const divkey = document.getElementById( 'k' +id);
-        audio.currenTime = 0;
-        audio.play();
-        divkey.className='active';
+function playS ( kc ){
+    const audio = document.querySelector( 'audio[data-key="'+ kc +'"]'  );
+    const divkey = document.querySelector( 'div[data-key="' + kc + '"]' );
+    if ( !audio ) { return; }
+    if ( !divkey ) { return; }
+    audio.currentTime = 0;
+    audio.play();
+    divkey.className = 'active';
 }
 
-function clearS( id ) {
-    const divkey = document.getElementById( 'k' +id);
+function clearS ( kc ) {
+    const divkey = document.querySelector( 'div[data-key="' + kc + '"]'  );
+    if ( !divkey ) { return; }
     divkey.className = '';
 }
 
-
-
 function myKeysDw( e ) {
-    switch ( e.keyCode ) {
-         case 65: playS('01' );
-         case 83: playS('02' );
-         case 68: playS('03' );
-         case 70: playS('04' );
-         case 71: playS('05' );
-             break;
-           default:  console.log( e );
-             break;
-     }
- }
+    playS( e.keyCode );
+}
 
- function myKeysUp( e ) {
-    switch( e.keyCode ) {
-        case 65: clearS('01' ); break;
-        case 83: clearS('02' ); break;
-        default:  console.log( e ); break;
+    function myKeysUp( e ) {
+        clearS( e.keyCode );
     }
- }
- 
+
---
+++ public/orchestra.old.js
@@ -0,0 +1,36 @@
+function playS( id ) {
+        const audio = document.getElementById( 'snd' +id );
+        const divkey = document.getElementById( 'k' +id);
+        audio.currenTime = 0;
+        audio.play();
+        divkey.className='active';
+}
+
+function clearS( id ) {
+    const divkey = document.getElementById( 'k' +id);
+    divkey.className = '';
+}
+
+
+
+function myKeysDw( e ) {
+    switch ( e.keyCode ) {
+         case 65: playS('01' );
+         case 83: playS('02' );
+         case 68: playS('03' );
+         case 70: playS('04' );
+         case 71: playS('05' );
+             break;
+           default:  console.log( e );
+             break;
+     }
+ }
+
+ function myKeysUp( e ) {
+    switch( e.keyCode ) {
+        case 65: clearS('01' ); break;
+        case 83: clearS('02' ); break;
+        default:  console.log( e ); break;
+    }
+ }
+ 
